4d2a7c60c262c33795dd5cd42cdda7111a7ebd1a
JAPHETH <japho0740@gmail.com>
2023-10-12 12:59:12 +0300

    Changes on the Model folder

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Model that defines the Storage component"""
 
+from models.engine.file_storage import FileStorage
 
+storage = FileStorage()
+storage.reload()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""serializes instances to a JSON file 
+and deserializes JSON file to instances
+"""
+import os
+import json
