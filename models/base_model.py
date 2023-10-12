9b0e8ca00e89ca9ab6fc561272d2fdc5caea6243
JAPHETH <japho0740@gmail.com>
2023-10-12 11:23:25 +0300

    Updated BaseModel module

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Script defines all common attributes/methods for other classes"""
+"""Script that defines all common
+attributes/methods for other classes
+"""
 
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """the BaseModel to HBnB clone"""
@@ -19,4 +22,36 @@ class BaseModel:
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
     
-    
+    def __init__(self, *args, **kwargs):
+        """Initializes instance attributes
+        Args:
+            - *args: arguments
+            - **kwargs: key-values arguments
+        """
+    if kwargs is not None and kwargs != {}:
+        for key_value in kwargs:
+            if key_value == "created_at":
+                """Merges "created_at" to a datetime object"""
+                self.__dict__["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            elif key_value == "updated_at":
+                """Merges "updated_at" to a datetime object"""
+                self.__dict__["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                """If the key is neither "created_at" 
+                nor "updated_at," set it as an attribute"""
+                self.__dict__[key_value] = kwargs[key_value]
+    else:
+        """Generate a new UUID as a string"""
+        self.id = str(uuid.uuid4()) 
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """return key values"""
+        A_dict = self.__dict__.copy()
+        A_dict["__class__"] = type(self).__name__
+        A_dict["created_at"] = A_dict["created_at"].isoformat()
+        A_dict["updated_at"] = A_dict["updated_at"].isoformat()
+        return A_dict
